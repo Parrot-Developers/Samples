dea99a4f7a6765853f7af1888dbef1606809ded8
Djavan Bertrand <djavan.bertrand@parrot.com>
2015-01-13 17:53:12 +0100

    [DEV] added streaming example

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 Samples
 =======
-
 This repo contains sample files to show you how to use the SDK. 
+
+Runnable samples
+---------------
+
+### JumpingSumoChangePosture (https://github.com/ARDroneSDK3/Samples/tree/master/Unix/JumpingSumoChangePosture)
+This example enables you to **connect** to a JumpingSumo and **send a command** to change its posture.
+
+### JumpingSumoReceiveStream(https://github.com/ARDroneSDK3/Samples/tree/master/Unix/JumpingSumoReceiveStream)
+This example enables you to **connect** to a JumpingSumo and **receive the video stream**.
+Two options are available : either display the stream (using ffplay) or store frames on the file system.
+
+Non runnable samples
+-------------------
+They are located in the Android and iOS folders.
+You can't compile them because of missing files. They are here to give you a full example on how to create the interface between drones and the controllers.
+
 The device controllers are used as an interface between the products and the controller.
 
 * MiniDroneDeviceController is used to control the RollingSpider
---
+++ Unix/JumpingSumoReceiveStream/JumpingSumoReceiveStream.h
@@ -0,0 +1,74 @@
+/*
+    Copyright (C) 2014 Parrot SA
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the 
+      distribution.
+    * Neither the name of Parrot nor the names
+      of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written
+      permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+    OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+    OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.
+*/
+#ifndef _SDK_EXAMPLE_JS_H_
+#define _SDK_EXAMPLE_JS_H_
+
+
+typedef struct
+{
+    ARNETWORKAL_Manager_t *alManager;
+    ARNETWORK_Manager_t *netManager;
+    ARSTREAM_Reader_t *streamReader;
+    ARSAL_Thread_t rxThread;
+    ARSAL_Thread_t txThread;
+    ARSAL_Thread_t videoTxThread;
+    ARSAL_Thread_t videoRxThread;
+    int d2cPort;
+    int c2dPort;
+    int arstreamFragSize;
+    int arstreamFragNb;
+    int arstreamAckDelay;
+    uint8_t *videoFrame;
+    uint32_t videoFrameSize;
+    
+    FILE *video_out;
+    int writeImgs;
+    int frameNb;
+    
+} JS_MANAGER_t;
+
+int ardiscoveryConnect (JS_MANAGER_t *jsManager);
+eARDISCOVERY_ERROR ARDISCOVERY_Connection_SendJsonCallback (uint8_t *dataTx, uint32_t *dataTxSize, void *customData);
+eARDISCOVERY_ERROR ARDISCOVERY_Connection_ReceiveJsonCallback (uint8_t *dataRx, uint32_t dataRxSize, char *ip, void *customData);
+
+int startNetwork (JS_MANAGER_t *jsManager);
+void onDisconnectNetwork (ARNETWORK_Manager_t *manager, ARNETWORKAL_Manager_t *alManager, void *customData);
+void stopNetwork (JS_MANAGER_t *jsManager);
+
+int startVideo (JS_MANAGER_t *jsManager);
+uint8_t *frameCompleteCallback (eARSTREAM_READER_CAUSE cause, uint8_t *frame, uint32_t frameSize, int numberOfSkippedFrames, int isFlushFrame, uint32_t *newBufferCapacity, void *custom);
+void stopVideo (JS_MANAGER_t *jsManager);
+
+int sendBeginStream(JS_MANAGER_t *jsManager);
+
+eARNETWORK_MANAGER_CALLBACK_RETURN arnetworkCmdCallback(int buffer_id, uint8_t *data, void *custom, eARNETWORK_MANAGER_CALLBACK_STATUS cause);
+
+#endif /* _SDK_EXAMPLE_JS_H_ */
