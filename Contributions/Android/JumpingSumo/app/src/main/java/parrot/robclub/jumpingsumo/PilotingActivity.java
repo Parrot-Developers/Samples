854cf8d8b596ab3ae9e0e5839f1af0eaf62196e3
Khai Nguyen Quoc <nqkhai1706@gmail.com>
2015-07-31 11:56:10 +0700

    Jumping Sumo Piloting Sample

---
+++ Contributions/Android/JumpingSumo/app/src/main/java/parrot/robclub/jumpingsumo/PilotingActivity.java
@@ -0,0 +1,476 @@
+package parrot.robclub.jumpingsumo;
+
+import java.io.ByteArrayInputStream;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parrot.arsdk.arcommands.ARCOMMANDS_JUMPINGSUMO_ANIMATIONS_JUMP_TYPE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DICTIONARY_KEY_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_ERROR_ENUM;
+import com.parrot.arsdk.arcontroller.ARControllerArgumentDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerException;
+import com.parrot.arsdk.arcontroller.ARDeviceController;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerListener;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerStreamListener;
+import com.parrot.arsdk.arcontroller.ARFrame;
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDevice;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceNetService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryException;
+
+/**
+ * Author: nguyenquockhai (nqkhai1706@gmail.com) create on 16/07/2015 at Robotics Club.
+ * Desc: This class is just based on PilotingActivity of BebopPilotingNewAPI project
+ * Addition, this project focus on receive stream video MJpeg of Jumping Sumo
+ */
+public class PilotingActivity extends Activity implements ARDeviceControllerListener, ARDeviceControllerStreamListener{
+    private static String TAG = PilotingActivity.class.getSimpleName();
+    public static String EXTRA_DEVICE_SERVICE = "pilotingActivity.extra.device.service";
+
+    public ARDeviceController deviceController;
+    public ARDiscoveryDeviceService service;
+    public ARDiscoveryDevice device;
+
+    private Button jumHightBt;
+    private Button jumLongBt;
+
+    private Button turnLeftBt;
+    private Button turnRightBt;
+
+    private Button forwardBt;
+    private Button backBt;
+
+    private TextView batteryLabel;
+
+    private AlertDialog alertDialog;
+
+    private ImageView imgView;
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piloting);
+
+        initIHM();
+        initVideo();
+
+        Intent intent = getIntent();
+        service = intent.getParcelableExtra(EXTRA_DEVICE_SERVICE);
+
+        //create the device
+        try
+        {
+            device = new ARDiscoveryDevice();
+
+            ARDiscoveryDeviceNetService netDeviceService = (ARDiscoveryDeviceNetService) service.getDevice();
+
+            device.initWifi(ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_JS, netDeviceService.getName(), netDeviceService.getIp(), netDeviceService.getPort());
+        }
+        catch (ARDiscoveryException e)
+        {
+            e.printStackTrace();
+            Log.e(TAG, "Error: " + e.getError());
+        }
+
+        if (device != null)
+        {
+            try
+            {
+                //create the deviceController
+                deviceController = new ARDeviceController (device);
+                deviceController.addListener (this);
+                deviceController.addStreamListener(this);
+            }
+            catch (ARControllerException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void initIHM ()
+    {
+        jumHightBt = (Button) findViewById(R.id.jumHightBt);
+        jumHightBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null) {
+                            deviceController.getFeatureJumpingSumo().sendAnimationsJump(ARCOMMANDS_JUMPINGSUMO_ANIMATIONS_JUMP_TYPE_ENUM.ARCOMMANDS_JUMPINGSUMO_ANIMATIONS_JUMP_TYPE_HIGH);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        jumLongBt = (Button) findViewById(R.id.jumLongBt);
+        jumLongBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null) {
+                            deviceController.getFeatureJumpingSumo().sendAnimationsJump(ARCOMMANDS_JUMPINGSUMO_ANIMATIONS_JUMP_TYPE_ENUM.ARCOMMANDS_JUMPINGSUMO_ANIMATIONS_JUMP_TYPE_LONG);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+
+        turnRightBt = (Button) findViewById(R.id.turnRightBt);
+        turnRightBt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDTurn((byte) 50);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 1);
+
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDTurn((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        turnLeftBt = (Button) findViewById(R.id.turnLeftBt);
+        turnLeftBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDTurn((byte) -50);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDTurn((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        forwardBt = (Button) findViewById(R.id.forwardBt);
+        forwardBt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDSpeed((byte) 50);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDSpeed((byte) 0);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        backBt = (Button) findViewById(R.id.backBt);
+        backBt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        if (deviceController != null) {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDSpeed((byte) -50);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        if (deviceController != null)
+                        {
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDSpeed((byte) 0);
+                            deviceController.getFeatureJumpingSumo().setPilotingPCMDFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        batteryLabel = (TextView) findViewById(R.id.batteryLabel);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        //start the deviceController
+        if (deviceController != null)
+        {
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PilotingActivity.this);
+
+            // set title
+            alertDialogBuilder.setTitle("Connecting ...");
+
+
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+            ARCONTROLLER_ERROR_ENUM error = deviceController.start();
+
+            if (error != ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK)
+            {
+                finish();
+            }
+        }
+    }
+
+    private void stopDeviceController()
+    {
+        if (deviceController != null)
+        {
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PilotingActivity.this);
+
+            // set title
+            alertDialogBuilder.setTitle("Disconnecting ...");
+
+            // show it
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // create alert dialog
+                    alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+                    ARCONTROLLER_ERROR_ENUM error = deviceController.stop();
+
+                    if (error != ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK) {
+                        finish();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        if (deviceController != null)
+        {
+            deviceController.stop();
+        }
+
+        super.onStop();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        stopDeviceController();
+    }
+
+    public void onUpdateBattery(final int percent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                batteryLabel.setText(String.format("%d%%", percent));
+            }
+        });
+    }
+
+
+    @Override
+    public void onStateChanged (ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARCONTROLLER_ERROR_ENUM error)
+    {
+        Log.i(TAG, "onStateChanged ... newState:" + newState + " error: " + error);
+
+        switch (newState)
+        {
+            case ARCONTROLLER_DEVICE_STATE_RUNNING:
+                //The deviceController is started
+                Log.i(TAG, "ARCONTROLLER_DEVICE_STATE_RUNNING .....");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //alertDialog.hide();
+                        alertDialog.dismiss();
+                    }
+                });
+                deviceController.getFeatureJumpingSumo().sendMediaStreamingVideoEnable((byte)1);
+                break;
+
+            case ARCONTROLLER_DEVICE_STATE_STOPPED:
+                //The deviceController is stoped
+                Log.i(TAG, "ARCONTROLLER_DEVICE_STATE_STOPPED ....." );
+
+                deviceController.dispose();
+                deviceController = null;
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        //alertDialog.hide();
+                        alertDialog.dismiss();
+                        finish();
+                    }
+                });
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onCommandReceived(ARDeviceController deviceController, ARCONTROLLER_DICTIONARY_KEY_ENUM commandKey, ARControllerDictionary elementDictionary)
+    {
+        if (elementDictionary != null)
+        {
+            if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED)
+            {
+                ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+
+                if (args != null)
+                {
+                    Integer batValue = (Integer) args.get("arcontroller_dictionary_key_common_commonstate_batterystatechanged_percent");
+
+                    onUpdateBattery(batValue);
+                }
+            }
+        }
+        else
+        {
+            Log.e(TAG, "elementDictionary is null");
+        }
+    }
+
+    @Override
+    public void onFrameReceived(ARDeviceController deviceController, ARFrame frame)
+    {
+        if (!frame.isIFrame())
+            return;
+
+        byte[] data = frame.getByteData();
+        ByteArrayInputStream ins = new ByteArrayInputStream(data);
+        Bitmap bmp = BitmapFactory.decodeStream(ins);
+
+        FrameDisplay fDisplay = new FrameDisplay(imgView, bmp);
+        fDisplay.execute();
+    }
+
+
+    @Override
+    public void onFrameTimeout(ARDeviceController deviceController)
+    {
+        Log.i(TAG, "onFrameTimeout ..... ");
+    }
+
+    //region video
+    public void initVideo()
+    {
+        imgView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    //endregion video
+}
+
+
+
