74a364d6f0d8ce574b4bc0609ea1e74e9eb30548
Pierre Zurek <pierre.zurek@parrot.com>
2015-10-08 12:30:44 +0200

    [DEV] Save changes in Contributions/Android
    Change-Id: I16ad12510857ecb3d2ba706ae1cc2b53f89299e7

--- Contributions/Android/JumpingSumo/app/src/main/java/parrot/robclub/jumpingsumo/MainActivity.java
+++ Contributions/Android/JumpingSumo/app/src/main/java/parrot/robclub/jumpingsumo/MainActivity.java
@@ -22,6 +22,7 @@ import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
 import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
 import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
 import com.parrot.arsdk.arsal.ARSALPrint;
+import com.parrot.arsdk.arsal.ARSAL_PRINT_LEVEL_ENUM;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +52,7 @@ public class MainActivity extends ActionBarActivity implements ARDiscoveryServic
             System.loadLibrary("arcontroller");
             System.loadLibrary("arcontroller_android");
 
-            ARSALPrint.enableDebugPrints();
+            ARSALPrint.setMinimumLogLevel(ARSAL_PRINT_LEVEL_ENUM.ARSAL_PRINT_DEBUG);
         }
         catch (Exception e)
         {
