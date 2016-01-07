51c478f151de9a6e7aa9d9eb635f4553255c6ded
Djavan Bertrand <djavan.bertrand@parrot.com>
2016-01-07 17:10:01 +0100

    [DEBUG] (iOS) Fixed drone not seen when coming back to app
    Also fixed compilation error on RSPilotingNewAPI
    And send streaming enable at connection with a Bebop

--- iOS/RSPilotingNewAPI/RSPilotingNewAPI/RSPilotingNewAPI/ViewController.m
+++ iOS/RSPilotingNewAPI/RSPilotingNewAPI/RSPilotingNewAPI/ViewController.m
@@ -85,12 +85,30 @@
 
 - (void)registerApplicationNotifications
 {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(enteredBackground:) name: UIApplicationDidEnterBackgroundNotification object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(enterForeground:) name: UIApplicationWillEnterForegroundNotification object: nil];
+    
+    
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(discoveryDidUpdateServices:) name:kARDiscoveryNotificationServicesDevicesListUpdated object:nil];
 }
 
 - (void)unregisterApplicationNotifications
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name: UIApplicationDidEnterBackgroundNotification object: nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name: UIApplicationWillEnterForegroundNotification object: nil];
+    
     [[NSNotificationCenter defaultCenter] removeObserver:self name:kARDiscoveryNotificationServicesDevicesListUpdated object:nil];
+}
+
+#pragma mark - application notifications
+- (void)enteredBackground:(NSNotification*)notification
+{
+    [[ARDiscovery sharedInstance] stop];
+}
+
+- (void)enterForeground:(NSNotification*)notification
+{
+    [[ARDiscovery sharedInstance] start];
 }
 
 #pragma mark ARDiscovery notification
